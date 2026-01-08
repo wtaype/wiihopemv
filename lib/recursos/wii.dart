3d232ef8d615bce2da553845fa60439aae2020d9
wtaype <wilder.taype16@gmail.com>
2026-01-07 22:19:19 -0500

    Actualizacion Principal v9.10.10

---
+++ lib/recursos/wii.dart
@@ -0,0 +1,17 @@
+class wii {
+  static const String app = 'WiiHope';
+  static const int lanzamiento = 2024;
+  static const String autor = '@wilder.taype';
+  static const String link = 'https://wtaype.github.io/';
+  static const String version = 'v9';
+}
+
+/** Actualizar main luego esto, pero si es mucho, solo esto. (1)
+git tag v9 -m "Version v9" ; git push origin v9
+
+//  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
+git add . ; git commit -m "Actualizacion Principal v9.10.10" ; git push origin main
+
+// En caso de emergencia, para actualizar el Tag existente. (3)
+git tag -d v9 ; git tag v9 -m "Version v9 actualizada" ; git push origin v9 --force
+ ACTUALIZACION TAG[END] */
