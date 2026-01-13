f712429f025df70d10c5ab6ed2eaa44ddea3ef9f
wtaype <wilder.taype16@gmail.com>
2026-01-12 23:48:37 -0500

    Actualizacion Principal v14.10.10

--- lib/wii.dart
+++ lib/wii.dart
@@ -3,15 +3,15 @@ class wii {
   static const int lanzamiento = 2024;
   static const String autor = '@wilder.taype';
   static const String link = 'https://wtaype.github.io/';
-  static const String version = 'v13.1.1';
+  static const String version = 'v14.1.1';
 }
 
 /** Actualizar main luego esto, pero si es mucho, solo esto. (1)
-git tag v13 -m "Version v13" ; git push origin v13
+git tag v14 -m "Version v14" ; git push origin v14
 
 //  ACTUALIZACIÓN PRINCIPAL ONE DEV [START] (2)
-git add . ; git commit -m "Actualizacion Principal v13.10.10" ; git push origin main
+git add . ; git commit -m "Actualizacion Principal v14.10.10" ; git push origin main
 
 // En caso de emergencia, para actualizar el Tag existente. (3)
-git tag -d v13 ; git tag v13 -m "Version v13 actualizada" ; git push origin v11 --force
+git tag -d v14 ; git tag v14 -m "Version v14 actualizada" ; git push origin v11 --force
  ACTUALIZACION TAG[END] */
